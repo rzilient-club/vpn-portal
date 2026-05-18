9f7d25b765530f6a80fec3d6c30555b95304ad44
duard <eduard@rzilient.club>
2026-05-18 11:47:13 +0200

    Pass vpn-update path from Go

--- version.go
+++ version.go
@@ -104,22 +104,22 @@ func handleAdminUpdate(w http.ResponseWriter, r *http.Request) {
 	updateRunning = true
 	updateMu.Unlock()
 
-	go func() {
-		defer func() {
-			updateMu.Lock()
-			updateRunning = false
-			updateMu.Unlock()
-		}()
-		log.Printf("[update] starting /usr/local/bin/vpn-update")
-		cmd := exec.Command("bash", "-c", "nohup /usr/local/bin/vpn-update > /var/log/vpn-update.log 2>&1 &")
-		cmd.Env = append(os.Environ(), "PATH=/usr/local/bin:/usr/bin:/bin:/sbin")
-		err := cmd.Run()
-		if err != nil {
-			log.Printf("[update] failed to start: %v", err)
-		} else {
-			log.Printf("[update] script launched in background")
-		}
-	}()
+	// Run completely detached from this process so it survives container death
+	cmd := exec.Command("setsid", "/usr/local/bin/vpn-update")
+	cmd.Env = append(os.Environ(), "PATH=/usr/local/bin:/usr/bin:/bin:/sbin")
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+	cmd.Stdin = nil
+	err := cmd.Start() // Start only, don't wait
+	if err != nil {
+		log.Printf("[update] failed to start: %v", err)
+		updateMu.Lock()
+		updateRunning = false
+		updateMu.Unlock()
+	} else {
+		log.Printf("[update] script detached with pid %d", cmd.Process.Pid)
+		cmd.Process.Release() // Detach from Go process
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, `{"status":"update started"}`)
